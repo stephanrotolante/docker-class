2555f73c3b49ad84cd1c389afc353b2f67789002
stephanrotolante <stephanrotolante@gmail.com>
2019-12-26 17:06:14 -0500

    ......

--- Section 3/redis-image/simpleweb/index.js
+++ Section 3/redis-image/simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req,res) => {
-    res.send("Fuck boy you know it");
+    res.send("Test");
 });
 
 app.listen(8080, () => {
